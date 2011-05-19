3b5c58a9ca16346d701a8bdb9a67361bb624c7c2
dgraham <david.malcom.graham@gmail.com>
2011-05-18 22:11:22 -0600

    Add http auth state tests.

---
+++ test/stream/http/auth_test.rb
@@ -0,0 +1,68 @@
+# encoding: UTF-8
+
+require 'vines'
+require 'minitest/autorun'
+
+class HttpAuthTest < MiniTest::Unit::TestCase
+  def setup
+    @stream = MiniTest::Mock.new
+    @state = Vines::Stream::Http::Auth.new(@stream, nil)
+  end
+
+  def test_missing_body_raises_error
+    node = node('<presence type="unavailable"/>')
+    assert_raises(Vines::StreamErrors::NotAuthorized) { @state.node(node) }
+  end
+
+  def test_body_with_missing_namespace_raises_error
+    node = node('<body rid="42" sid="12"/>')
+    assert_raises(Vines::StreamErrors::NotAuthorized) { @state.node(node) }
+  end
+
+  def test_missing_rid_raises_error
+    node = node('<body xmlns="http://jabber.org/protocol/httpbind" sid="12"/>')
+    assert_raises(Vines::StreamErrors::NotAuthorized) { @state.node(node) }
+  end
+
+  def test_invalid_session_raises_error
+    @stream.expect(:valid_session?, false, ['12'])
+    node = node('<body xmlns="http://jabber.org/protocol/httpbind" rid="42" sid="12"/>')
+    assert_raises(Vines::StreamErrors::NotAuthorized) { @state.node(node) }
+    assert @stream.verify
+  end
+
+  def test_empty_body_raises_error
+    node = node('<body xmlns="http://jabber.org/protocol/httpbind" rid="42" sid="12"/>')
+    @stream.expect(:valid_session?, true, ['12'])
+    @stream.expect(:parse_body, [], [node])
+    assert_raises(Vines::StreamErrors::NotAuthorized) { @state.node(node) }
+    assert @stream.verify
+  end
+
+  def test_body_with_two_children_raises_error
+    node = node('<body xmlns="http://jabber.org/protocol/httpbind" rid="42" sid="12"><message/><message/></body>')
+    message = node('<message/>')
+    @stream.expect(:valid_session?, true, ['12'])
+    @stream.expect(:parse_body, [message, message], [node])
+    assert_raises(Vines::StreamErrors::NotAuthorized) { @state.node(node) }
+    assert @stream.verify
+  end
+
+  def test_valid_body_processes
+    auth = node(%Q{<auth xmlns="#{Vines::NAMESPACES[:sasl]}" mechanism="PLAIN"/>})
+    node = node('<body xmlns="http://jabber.org/protocol/httpbind" rid="42" sid="12"></body>')
+    node << auth
+    @stream.expect(:valid_session?, true, ['12'])
+    @stream.expect(:parse_body, [auth], [node])
+    # this error means we correctly called the parent method Client#node
+    @stream.expect(:error, nil, [Vines::SaslErrors::MalformedRequest.new])
+    @state.node(node)
+    assert @stream.verify
+  end
+
+  private
+
+  def node(xml)
+    Nokogiri::XML(xml).root
+  end
+end
