6518f3d581c699b825b22b503e9a00d59c08b527
David Graham <dgraham@github.com>
2012-09-13 04:01:46 -0600

    Convert to specs.

--- test/stanza/pubsub/delete_test.rb
+++ test/stanza/pubsub/delete_test.rb
@@ -1,142 +1,169 @@
 # encoding: UTF-8
 
-require 'tmpdir'
-require 'vines'
-require 'ext/nokogiri'
-require 'minitest/autorun'
+require 'test_helper'
 
-class DeletePubSubTest < MiniTest::Unit::TestCase
-  def setup
-    @user = Vines::User.new(jid: 'alice@wonderland.lit/tea')
-    @config = Vines::Config.new do
+describe Vines::Stanza::PubSub::Delete do
+  subject      { Vines::Stanza::PubSub::Delete.new(xml, stream) }
+  let(:alice)  { Vines::User.new(jid: 'alice@wonderland.lit/tea') }
+  let(:stream) { MiniTest::Mock.new }
+  let(:config) do
+    Vines::Config.new do
       host 'wonderland.lit' do
         storage(:fs) { dir Dir.tmpdir }
         pubsub 'games'
       end
     end
-    @stream = MiniTest::Mock.new
-    @stream.expect(:config, @config)
-    @stream.expect(:user, @user)
   end
 
-  def test_missing_to_address_raises
-    node = node(%q{
-      <iq type='set' id='42'>
-        <pubsub xmlns='http://jabber.org/protocol/pubsub'>
-          <delete node='game_13'/>
-        </pubsub>
-      </iq>
-    }.strip.gsub(/\n|\s{2,}/, ''))
-
-    @stream.expect(:domain, 'wonderland.lit')
-
-    stanza = Vines::Stanza::PubSub::Delete.new(node, @stream)
-    assert_raises(Vines::StanzaErrors::FeatureNotImplemented) { stanza.process }
-    assert @stream.verify
-  end
-
-  def test_server_domain_to_address_raises
-    node = node(%q{
-      <iq type='set' to='wonderland.lit' id='42'>
-        <pubsub xmlns='http://jabber.org/protocol/pubsub'>
-          <delete node='game_13'/>
-        </pubsub>
-      </iq>
-    }.strip.gsub(/\n|\s{2,}/, ''))
-
-    stanza = Vines::Stanza::PubSub::Delete.new(node, @stream)
-    assert_raises(Vines::StanzaErrors::FeatureNotImplemented) { stanza.process }
-    assert @stream.verify
-  end
-
-  def test_non_pubsub_to_address_routes
-    node = node(%q{
-      <iq type='set' to='bogus.wonderland.lit' id='42'>
-        <pubsub xmlns='http://jabber.org/protocol/pubsub'>
-          <delete node='game_13'/>
-        </pubsub>
-      </iq>
-    }.strip.gsub(/\n|\s{2,}/, ''))
-
-    router = MiniTest::Mock.new
-    router.expect(:route, nil, [node])
-    @stream.expect(:router, router)
-
-    stanza = Vines::Stanza::PubSub::Delete.new(node, @stream)
-    stanza.process
-    assert @stream.verify
-    assert router.verify
-  end
-
-  def test_multiple_delete_elements_raises
-    node = node(%q{
-      <iq type='set' to='games.wonderland.lit' id='42'>
-        <pubsub xmlns='http://jabber.org/protocol/pubsub'>
-          <delete node='game_13'/>
-          <delete node='game_14'/>
-        </pubsub>
-      </iq>
-    }.strip.gsub(/\n|\s{2,}/, ''))
-
-    stanza = Vines::Stanza::PubSub::Delete.new(node, @stream)
-    assert_raises(Vines::StanzaErrors::BadRequest) { stanza.process }
-    assert @stream.verify
-  end
-
-  def test_delete_missing_node_raises
-    node = node(%q{
-      <iq type='set' to='games.wonderland.lit' id='42'>
-        <pubsub xmlns='http://jabber.org/protocol/pubsub'>
-          <delete node='game_13'/>
-        </pubsub>
-      </iq>
-    }.strip.gsub(/\n|\s{2,}/, ''))
-
-    stanza = Vines::Stanza::PubSub::Delete.new(node, @stream)
-    assert_raises(Vines::StanzaErrors::ItemNotFound) { stanza.process }
-    assert @stream.verify
-  end
-
-  def test_good_stanza_processes
-    node = node(%q{
-      <iq type='set' to='games.wonderland.lit' id='42'>
-        <pubsub xmlns='http://jabber.org/protocol/pubsub'>
-          <delete node='game_13'/>
-        </pubsub>
-      </iq>
-    }.strip.gsub(/\n|\s{2,}/, ''))
-
-    def @stream.nodes; @nodes; end
-    def @stream.write(node)
-      @nodes ||= []
-      @nodes << node
-    end
-
-    stanza = Vines::Stanza::PubSub::Delete.new(node, @stream)
-    def stanza.mock_pubsub; @mock_pubsub; end
-    def stanza.pubsub
-      unless @mock_pubsub
-        xml = %q{<message><event xmlns="http://jabber.org/protocol/pubsub#event"><delete node="game_13"/></event></message>}
-        @mock_pubsub = MiniTest::Mock.new
-        @mock_pubsub.expect(:node?, true, ['game_13'])
-        @mock_pubsub.expect(:publish, nil, ['game_13', Nokogiri::XML(xml).root])
-        @mock_pubsub.expect(:delete_node, nil, ['game_13'])
+  before do
+    class << stream
+      attr_accessor :config, :domain, :nodes, :user
+      def write(node)
+        @nodes ||= []
+        @nodes << node
       end
-      @mock_pubsub
     end
-    stanza.process
-
-    assert @stream.verify
-    assert stanza.mock_pubsub.verify
-    assert_equal 1, @stream.nodes.size
-
-    expected = node(%q{<iq from="games.wonderland.lit" id="42" to="alice@wonderland.lit/tea" type="result"/>})
-    assert_equal expected, @stream.nodes[0]
+    stream.config = config
+    stream.domain = 'wonderland.lit'
+    stream.user = alice
   end
 
-  private
+  describe 'when missing a to address' do
+    let(:xml) do
+      node(%q{
+        <iq type='set' id='42'>
+          <pubsub xmlns='http://jabber.org/protocol/pubsub'>
+            <delete node='game_13'/>
+          </pubsub>
+        </iq>
+      })
+    end
 
-  def node(xml)
-    Nokogiri::XML(xml).root
+    it 'raises a feature-not-implemented stanza error' do
+      -> { subject.process }.must_raise Vines::StanzaErrors::FeatureNotImplemented
+      stream.verify
+    end
+  end
+
+  describe 'when addressed to a bare server domain jid' do
+    let(:xml) do
+      node(%q{
+        <iq type='set' to='wonderland.lit' id='42'>
+          <pubsub xmlns='http://jabber.org/protocol/pubsub'>
+            <delete node='game_13'/>
+          </pubsub>
+        </iq>
+      })
+    end
+
+    it 'raises a feature-not-implemented stanza error' do
+      -> { subject.process }.must_raise Vines::StanzaErrors::FeatureNotImplemented
+      stream.verify
+    end
+  end
+
+  describe 'when addressed to a non-pubsub address' do
+    let(:router) { MiniTest::Mock.new }
+    let(:xml) do
+      node(%q{
+        <iq type='set' to='bogus.wonderland.lit' id='42'>
+          <pubsub xmlns='http://jabber.org/protocol/pubsub'>
+            <delete node='game_13'/>
+          </pubsub>
+        </iq>
+      })
+    end
+
+    before do
+      router.expect :route, nil, [xml]
+      stream.expect :router, router
+    end
+
+    it 'routes rather than handle locally' do
+      subject.process
+      stream.verify
+      router.verify
+    end
+  end
+
+  describe 'when stanza contains multiple delete elements' do
+    let(:xml) do
+      node(%q{
+        <iq type='set' to='games.wonderland.lit' id='42'>
+          <pubsub xmlns='http://jabber.org/protocol/pubsub'>
+            <delete node='game_13'/>
+            <delete node='game_14'/>
+          </pubsub>
+        </iq>
+      })
+    end
+
+    it 'raises a bad-request stanza error' do
+      -> { subject.process }.must_raise Vines::StanzaErrors::BadRequest
+      stream.verify
+    end
+  end
+
+  describe 'when deleting a missing node' do
+    let(:xml) do
+      node(%q{
+        <iq type='set' to='games.wonderland.lit' id='42'>
+          <pubsub xmlns='http://jabber.org/protocol/pubsub'>
+            <delete node='game_13'/>
+          </pubsub>
+        </iq>
+      })
+    end
+
+    it 'raises an item-not-found stanza error' do
+      -> { subject.process }.must_raise Vines::StanzaErrors::ItemNotFound
+      stream.verify
+    end
+  end
+
+  describe 'when valid stanza is received' do
+    let(:pubsub) { MiniTest::Mock.new }
+    let(:xml) do
+      node(%q{
+        <iq type='set' to='games.wonderland.lit' id='42'>
+          <pubsub xmlns='http://jabber.org/protocol/pubsub'>
+            <delete node='game_13'/>
+          </pubsub>
+        </iq>
+      })
+    end
+
+    let(:result) { node(%Q{<iq from="games.wonderland.lit" id="42" to="#{alice.jid}" type="result"/>}) }
+
+    let(:broadcast) do
+      node(%q{
+        <message>
+          <event xmlns="http://jabber.org/protocol/pubsub#event">
+            <delete node="game_13"/>
+          </event>
+        </message>})
+    end
+
+    before do
+      pubsub.expect :node?, true, ['game_13']
+      pubsub.expect :publish, nil, ['game_13', broadcast]
+      pubsub.expect :delete_node, nil, ['game_13']
+    end
+
+    it 'broadcasts the delete to subscribers' do
+      subject.stub :pubsub, pubsub do
+        subject.process
+      end
+      stream.verify
+      pubsub.verify
+    end
+
+    it 'sends a result stanza to sender' do
+      subject.stub :pubsub, pubsub do
+        subject.process
+      end
+      stream.nodes.size.must_equal 1
+      stream.nodes.first.must_equal result
+    end
   end
 end
