bfc778794550ddf61cc9e4c52436995556c09661
David Graham <dgraham@github.com>
2012-09-14 23:53:11 +0400

    Convert to specs.

--- test/stanza/iq/roster_test.rb
+++ test/stanza/iq/roster_test.rb
@@ -1,206 +1,230 @@
 # encoding: UTF-8
 
-require 'vines'
-require 'ext/nokogiri'
-require 'minitest/autorun'
+require 'test_helper'
 
-class RosterTest < MiniTest::Unit::TestCase
-  def setup
-    @stream = MiniTest::Mock.new
+describe Vines::Stanza::Iq::Roster do
+  subject      { Vines::Stanza::Iq::Roster.new(xml, stream) }
+  let(:stream) { MiniTest::Mock.new }
+# FIXME try without resource
+  let(:alice)  { Vines::User.new(jid: 'alice@wonderland.lit/tea') }
+
+  before do
+    class << stream
+      attr_accessor :domain, :user
+    end
+    stream.user = alice
+    stream.domain = 'wonderland.lit'
   end
 
-  def test_roster_get_with_empty_roster
-    alice = Vines::User.new(:jid => 'alice@wonderland.lit')
-    expected = node(%q{<iq id="42" type="result"><query xmlns="jabber:iq:roster"/></iq>})
-    @stream.expect(:write, nil, [expected])
-    @stream.expect(:requested_roster!, nil)
-    @stream.expect(:user, alice)
+  describe 'when retrieving an empty roster' do
+    let(:xml) { node(%q{<iq id="42" type="get"><query xmlns='jabber:iq:roster'/></iq>}) }
+    let(:expected) { node(%q{<iq id="42" type="result"><query xmlns="jabber:iq:roster"/></iq>}) }
 
-    node = node(%q{<iq id="42" type="get"><query xmlns='jabber:iq:roster'/></iq>})
-    stanza = Vines::Stanza::Iq::Roster.new(node, @stream)
-    stanza.process
-    assert @stream.verify
-  end
-
-  def test_roster_get_with_non_empty_roster
-    alice = Vines::User.new(:jid => 'alice@wonderland.lit')
-    alice.roster << Vines::Contact.new(:jid => 'hatter@wonderland.lit')
-    alice.roster << Vines::Contact.new(:jid => 'cat@wonderland.lit', :groups => ['Friends', 'Cats'])
-
-    expected = node(%q{
-      <iq id="42" type="result">
-        <query xmlns="jabber:iq:roster">
-          <item jid="cat@wonderland.lit" subscription="none">
-            <group>Cats</group>
-            <group>Friends</group>
-          </item>
-          <item jid="hatter@wonderland.lit" subscription="none"/>
-        </query>
-      </iq>}.strip.gsub(/\n|\s{2,}/, ''))
-
-    @stream.expect(:write, nil, [expected])
-    @stream.expect(:requested_roster!, nil)
-    @stream.expect(:user, alice)
-
-    node = node(%q{<iq id="42" type="get"><query xmlns='jabber:iq:roster'/></iq>})
-    stanza = Vines::Stanza::Iq::Roster.new(node, @stream)
-    stanza.process
-    assert @stream.verify
-  end
-
-  def test_roster_get_with_invalid_to_address
-    alice = Vines::User.new(:jid => 'alice@wonderland.lit/tea')
-    @stream.expect(:user, alice)
-
-    node = node(%q{
-      <iq id="42" type="get" to="romeo@verona.lit">
-        <query xmlns="jabber:iq:roster"/>
-      </iq>}.strip.gsub(/\n|\s{2,}/, ''))
-
-    stanza = Vines::Stanza::Iq::Roster.new(node, @stream)
-    assert_raises(Vines::StanzaErrors::Forbidden) { stanza.process }
-    assert @stream.verify
-  end
-
-  def test_roster_set_with_invalid_to_address
-    alice = Vines::User.new(:jid => 'alice@wonderland.lit/tea')
-    @stream.expect(:user, alice)
-
-    node = node(%q{
-      <iq id="42" type="set" to="romeo@verona.lit">
-        <query xmlns="jabber:iq:roster"/>
-      </iq>}.strip.gsub(/\n|\s{2,}/, ''))
-
-    stanza = Vines::Stanza::Iq::Roster.new(node, @stream)
-    assert_raises(Vines::StanzaErrors::Forbidden) { stanza.process }
-    assert @stream.verify
-  end
-
-  def test_roster_set_with_no_items
-    node = node(%q{
-      <iq id="42" type="set">
-        <query xmlns="jabber:iq:roster"/>
-      </iq>}.strip.gsub(/\n|\s{2,}/, ''))
-
-    stanza = Vines::Stanza::Iq::Roster.new(node, @stream)
-    assert_raises(Vines::StanzaErrors::BadRequest) { stanza.process }
-    assert @stream.verify
-  end
-
-  def test_roster_set_with_two_items
-    node = node(%q{
-      <iq id="42" type="set">
-        <query xmlns="jabber:iq:roster">
-          <item jid="hatter@wonderland.lit"/>
-          <item jid="cat@wonderland.lit"/>
-        </query>
-      </iq>}.strip.gsub(/\n|\s{2,}/, ''))
-
-    stanza = Vines::Stanza::Iq::Roster.new(node, @stream)
-    assert_raises(Vines::StanzaErrors::BadRequest) { stanza.process }
-    assert @stream.verify
-  end
-
-  def test_roster_set_missing_jid
-    node = node(%q{
-      <iq id="42" type="set">
-        <query xmlns="jabber:iq:roster">
-          <item name="Mad Hatter"/>
-        </query>
-      </iq>}.strip.gsub(/\n|\s{2,}/, ''))
-
-    stanza = Vines::Stanza::Iq::Roster.new(node, @stream)
-    assert_raises(Vines::StanzaErrors::BadRequest) { stanza.process }
-    assert @stream.verify
-  end
-
-  def test_roster_set_with_duplicate_groups
-    alice = Vines::User.new(:jid => 'alice@wonderland.lit/tea')
-    @stream.expect(:user, alice)
-
-    node = node(%q{
-      <iq id="42" type="set">
-        <query xmlns="jabber:iq:roster">
-          <item jid="hatter@wonderland.lit" name="Mad Hatter">
-            <group>Friends</group>
-            <group>Friends</group>
-          </item>
-        </query>
-      </iq>}.strip.gsub(/\n|\s{2,}/, ''))
-
-    stanza = Vines::Stanza::Iq::Roster.new(node, @stream)
-    assert_raises(Vines::StanzaErrors::BadRequest) { stanza.process }
-    assert @stream.verify
-  end
-
-  def test_roster_set_with_empty_group
-    alice = Vines::User.new(:jid => 'alice@wonderland.lit/tea')
-    @stream.expect(:user, alice)
-
-    node = node(%q{
-      <iq id="42" type="set">
-        <query xmlns="jabber:iq:roster">
-          <item jid="hatter@wonderland.lit" name="Mad Hatter">
-            <group></group>
-          </item>
-        </query>
-      </iq>}.strip.gsub(/\n|\s{2,}/, ''))
-
-    stanza = Vines::Stanza::Iq::Roster.new(node, @stream)
-    assert_raises(Vines::StanzaErrors::NotAcceptable) { stanza.process }
-    assert @stream.verify
-  end
-
-  def test_roster_set_sends_results
-    alice = Vines::User.new(:jid => 'alice@wonderland.lit/tea')
-    storage = MiniTest::Mock.new
-    storage.expect(:save_user, nil, [alice])
-
-    recipient = MiniTest::Mock.new
-    recipient.expect(:user, alice)
-    def recipient.nodes; @nodes; end
-    def recipient.write(node)
-      @nodes ||= []
-      @nodes << node
+    before do
+      stream.expect :write, nil, [expected]
+      stream.expect :requested_roster!, nil
     end
 
-    @stream.expect(:user, alice)
-    @stream.expect(:interested_resources, [recipient], [alice.jid])
-    @stream.expect(:update_user_streams, nil, [alice])
-    @stream.expect(:domain, 'wonderland.lit')
-    @stream.expect(:storage, storage, ['wonderland.lit'])
-    expected = node(%Q{<iq id="42" to="#{alice.jid}" type="result"/>})
-    @stream.expect(:write, nil, [expected])
-
-    node = node(%q{
-      <iq id="42" type="set">
-        <query xmlns="jabber:iq:roster">
-          <item jid="hatter@wonderland.lit" name="Mad Hatter">
-            <group>Friends</group>
-          </item>
-        </query>
-      </iq>}.strip.gsub(/\n|\s{2,}/, ''))
-
-    stanza = Vines::Stanza::Iq::Roster.new(node, @stream)
-    stanza.process
-    assert @stream.verify
-    assert storage.verify
-
-    expected = node(%q{
-      <iq to="alice@wonderland.lit/tea" type="set">
-        <query xmlns="jabber:iq:roster">
-          <item jid="hatter@wonderland.lit" name="Mad Hatter" subscription="none">
-            <group>Friends</group>
-          </item>
-        </query>
-      </iq>}.strip.gsub(/\n|\s{2,}/, ''))
-    recipient.nodes[0].remove_attribute('id') # id is random
-    assert_equal expected, recipient.nodes[0]
+    it 'returns an empty stanza' do
+      subject.process
+      stream.verify
+    end
   end
 
-  private
+  describe 'when retrieving a non-empty roster' do
+    let(:xml) { node(%q{<iq id="42" type="get"><query xmlns='jabber:iq:roster'/></iq>}) }
+    let(:expected) do
+      node(%q{
+        <iq id="42" type="result">
+          <query xmlns="jabber:iq:roster">
+            <item jid="cat@wonderland.lit" subscription="none">
+              <group>Cats</group>
+              <group>Friends</group>
+            </item>
+            <item jid="hatter@wonderland.lit" subscription="none"/>
+          </query>
+        </iq>})
+    end
 
-  def node(xml)
-    Nokogiri::XML(xml).root
+    before do
+      alice.roster << Vines::Contact.new(jid: 'hatter@wonderland.lit')
+      alice.roster << Vines::Contact.new(jid: 'cat@wonderland.lit', :groups => ['Friends', 'Cats'])
+
+      stream.expect :write, nil, [expected]
+      stream.expect :requested_roster!, nil
+    end
+
+    it 'sorts groups alphabetically' do
+      subject.process
+      stream.verify
+    end
+  end
+
+  describe 'when requesting a roster for another user' do
+    let(:xml) do
+      node(%q{
+        <iq id="42" type="get" to="romeo@verona.lit">
+          <query xmlns="jabber:iq:roster"/>
+        </iq>})
+    end
+
+    it 'raises a forbidden stanza error' do
+      -> { subject.process }.must_raise Vines::StanzaErrors::Forbidden
+      stream.verify
+    end
+  end
+
+  describe 'when saving a roster for another user' do
+    let(:xml) do
+      node(%q{
+        <iq id="42" type="set" to="romeo@verona.lit">
+          <query xmlns="jabber:iq:roster"/>
+        </iq>})
+    end
+
+    it 'raises a forbidden stanza error' do
+      -> { subject.process }.must_raise Vines::StanzaErrors::Forbidden
+      stream.verify
+    end
+  end
+
+  describe 'when saving a roster with no items' do
+    let(:xml) do
+      node(%q{
+        <iq id="42" type="set">
+          <query xmlns="jabber:iq:roster"/>
+        </iq>})
+    end
+
+    it 'raises a bad-request stanza error' do
+      -> { subject.process }.must_raise Vines::StanzaErrors::BadRequest
+      stream.verify
+    end
+  end
+
+  describe 'when updating a roster with more than one item' do
+    let(:xml) do
+      node(%q{
+        <iq id="42" type="set">
+          <query xmlns="jabber:iq:roster">
+            <item jid="hatter@wonderland.lit"/>
+            <item jid="cat@wonderland.lit"/>
+          </query>
+        </iq>})
+    end
+
+    it 'raises a bad-request stanza error' do
+      -> { subject.process }.must_raise Vines::StanzaErrors::BadRequest
+      stream.verify
+    end
+  end
+
+  describe 'when adding a roster item without a jid attribute' do
+    let(:xml) do
+      node(%q{
+        <iq id="42" type="set">
+          <query xmlns="jabber:iq:roster">
+            <item name="Mad Hatter"/>
+          </query>
+        </iq>})
+    end
+
+    it 'raises a bad-request stanza error' do
+      -> { subject.process }.must_raise Vines::StanzaErrors::BadRequest
+      stream.verify
+    end
+  end
+
+  describe 'when adding a roster item with duplicate groups' do
+    let(:xml) do
+      node(%q{
+        <iq id="42" type="set">
+          <query xmlns="jabber:iq:roster">
+            <item jid="hatter@wonderland.lit" name="Mad Hatter">
+              <group>Friends</group>
+              <group>Friends</group>
+            </item>
+          </query>
+        </iq>})
+    end
+
+    it 'raises a bad-request stanza error' do
+      -> { subject.process }.must_raise Vines::StanzaErrors::BadRequest
+      stream.verify
+    end
+  end
+
+  describe 'when adding a roster item with an empty group name' do
+    let(:xml) do
+      node(%q{
+        <iq id="42" type="set">
+          <query xmlns="jabber:iq:roster">
+            <item jid="hatter@wonderland.lit" name="Mad Hatter">
+              <group></group>
+            </item>
+          </query>
+        </iq>})
+    end
+
+    it 'raises a not-acceptable stanza error' do
+      -> { subject.process }.must_raise Vines::StanzaErrors::NotAcceptable
+      stream.verify
+    end
+  end
+
+  describe 'when saving a roster successfully' do
+    let(:xml) do
+      node(%q{
+        <iq id="42" type="set">
+          <query xmlns="jabber:iq:roster">
+            <item jid="hatter@wonderland.lit" name="Mad Hatter">
+              <group>Friends</group>
+            </item>
+          </query>
+        </iq>})
+    end
+
+    let(:expected) do
+      node(%q{
+        <iq to="alice@wonderland.lit/tea" type="set">
+          <query xmlns="jabber:iq:roster">
+            <item jid="hatter@wonderland.lit" name="Mad Hatter" subscription="none">
+              <group>Friends</group>
+            </item>
+          </query>
+        </iq>})
+    end
+
+    let(:storage) { MiniTest::Mock.new }
+    let(:recipient) { MiniTest::Mock.new }
+    let(:result) { node(%Q{<iq id="42" to="#{alice.jid}" type="result"/>}) }
+
+    before do
+      storage.expect :save_user, nil, [alice]
+
+      recipient.expect :user, alice
+      def recipient.nodes; @nodes; end
+      def recipient.write(node)
+        @nodes ||= []
+        @nodes << node
+      end
+
+      stream.expect :interested_resources, [recipient], [alice.jid]
+      stream.expect :update_user_streams, nil, [alice]
+      stream.expect :storage, storage, ['wonderland.lit']
+      stream.expect :write, nil, [result]
+    end
+
+    it 'sends a result to the sender' do
+      subject.process
+      stream.verify
+      storage.verify
+    end
+
+    it 'sends the new roster item to the interested streams' do
+      subject.process
+      recipient.nodes.first.remove_attribute('id') # id is random
+      recipient.nodes.first.must_equal expected
+    end
   end
 end
