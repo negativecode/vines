def86de5735168b3adb6dcaa68d48412c507b175
David Graham <dgraham@github.com>
2012-09-14 22:49:55 +0400

    Fix mock errors for MiniTest 3.

--- test/stanza/iq/disco_info_test.rb
+++ test/stanza/iq/disco_info_test.rb
@@ -1,78 +1,78 @@
 # encoding: UTF-8
 
-require 'tmpdir'
-require 'vines'
-require 'ext/nokogiri'
-require 'minitest/autorun'
+require 'test_helper'
 
-class DiscoInfoTest < MiniTest::Unit::TestCase
-  ALICE = Vines::User.new(:jid => 'alice@wonderland.lit/home')
-
-  def setup
-    @config = Vines::Config.new do
+describe Vines::Stanza::Iq::DiscoInfo do
+  subject      { Vines::Stanza::Iq::DiscoInfo.new(xml, stream) }
+  let(:stream) { MiniTest::Mock.new }
+  let(:alice)  { Vines::User.new(jid: 'alice@wonderland.lit/home') }
+  let(:config) do
+    Vines::Config.new do
       host 'wonderland.lit' do
         storage(:fs) { dir Dir.tmpdir }
       end
     end
-    @stream = MiniTest::Mock.new
-    @stream.expect(:user, ALICE)
-    @stream.expect(:config, @config)
   end
 
-  def test_private_storage_disabled
+  let(:xml) do
     query = %q{<query xmlns="http://jabber.org/protocol/disco#info"/>}
-    node = node(%Q{<iq id="42" to="wonderland.lit" type="get">#{query}</iq>})
-
-    expected = node(%Q{
-      <iq from="wonderland.lit" id="42" to="#{ALICE.jid}" type="result">
-        <query xmlns="http://jabber.org/protocol/disco#info">
-          <identity category="server" type="im"/>
-          <feature var="http://jabber.org/protocol/disco#info"/>
-          <feature var="http://jabber.org/protocol/disco#items"/>
-          <feature var="urn:xmpp:ping"/>
-          <feature var="vcard-temp"/>
-          <feature var="jabber:iq:version"/>
-        </query>
-      </iq>
-    }.strip.gsub(/\n|\s{2,}/, ''))
-
-    @config.vhost('wonderland.lit').private_storage false
-    @stream.expect(:write, nil, [expected])
-
-    stanza = Vines::Stanza::Iq::DiscoInfo.new(node, @stream)
-    stanza.process
-    assert @stream.verify
+    node(%Q{<iq id="42" to="wonderland.lit" type="get">#{query}</iq>})
   end
 
-  def test_private_storage_enabled
-    query = %q{<query xmlns="http://jabber.org/protocol/disco#info"/>}
-    node = node(%Q{<iq id="42" to="wonderland.lit" type="get">#{query}</iq>})
-
-    expected = node(%Q{
-      <iq from="wonderland.lit" id="42" to="#{ALICE.jid}" type="result">
-        <query xmlns="http://jabber.org/protocol/disco#info">
-          <identity category="server" type="im"/>
-          <feature var="http://jabber.org/protocol/disco#info"/>
-          <feature var="http://jabber.org/protocol/disco#items"/>
-          <feature var="urn:xmpp:ping"/>
-          <feature var="vcard-temp"/>
-          <feature var="jabber:iq:version"/>
-          <feature var="jabber:iq:private"/>
-        </query>
-      </iq>
-    }.strip.gsub(/\n|\s{2,}/, ''))
-
-    @config.vhost('wonderland.lit').private_storage true
-    @stream.expect(:write, nil, [expected])
-
-    stanza = Vines::Stanza::Iq::DiscoInfo.new(node, @stream)
-    stanza.process
-    assert @stream.verify
+  before do
+    class << stream
+      attr_accessor :config, :user
+    end
+    stream.config = config
+    stream.user = alice
   end
 
-  private
+  describe 'when private storage is disabled' do
+    let(:expected) do
+      node(%Q{
+        <iq from="wonderland.lit" id="42" to="#{alice.jid}" type="result">
+          <query xmlns="http://jabber.org/protocol/disco#info">
+            <identity category="server" type="im"/>
+            <feature var="http://jabber.org/protocol/disco#info"/>
+            <feature var="http://jabber.org/protocol/disco#items"/>
+            <feature var="urn:xmpp:ping"/>
+            <feature var="vcard-temp"/>
+            <feature var="jabber:iq:version"/>
+          </query>
+        </iq>
+      })
+    end
 
-  def node(xml)
-    Nokogiri::XML(xml).root
+    it 'returns info stanza without the private storage feature' do
+      config.vhost('wonderland.lit').private_storage false
+      stream.expect :write, nil, [expected]
+      subject.process
+      stream.verify
+    end
+  end
+
+  describe 'when private storage is enabled' do
+    let(:expected) do
+      node(%Q{
+        <iq from="wonderland.lit" id="42" to="#{alice.jid}" type="result">
+          <query xmlns="http://jabber.org/protocol/disco#info">
+            <identity category="server" type="im"/>
+            <feature var="http://jabber.org/protocol/disco#info"/>
+            <feature var="http://jabber.org/protocol/disco#items"/>
+            <feature var="urn:xmpp:ping"/>
+            <feature var="vcard-temp"/>
+            <feature var="jabber:iq:version"/>
+            <feature var="jabber:iq:private"/>
+          </query>
+        </iq>
+      })
+    end
+
+    it 'announces private storage feature in info stanza result' do
+      config.vhost('wonderland.lit').private_storage true
+      stream.expect :write, nil, [expected]
+      subject.process
+      stream.verify
+    end
   end
 end
