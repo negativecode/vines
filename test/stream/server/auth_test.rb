df6fd70e003c672b09fa4f94fc9a56b6aed4e995
David Graham <dgraham@github.com>
2012-09-17 07:31:49 +0400

    Fixes for MiniTest 3.4.

--- test/stream/server/auth_test.rb
+++ test/stream/server/auth_test.rb
@@ -1,57 +1,61 @@
 # encoding: UTF-8
 
-require 'vines'
-require 'minitest/autorun'
+require 'test_helper'
 
 describe Vines::Stream::Server::Auth do
   # disable logging for tests
   Class.new.extend(Vines::Log).log.level = Logger::FATAL
 
+  subject      { Vines::Stream::Server::Auth.new(stream) }
+  let(:stream) { MiniTest::Mock.new }
+
   before do
-    @stream = MiniTest::Mock.new
-    @state = Vines::Stream::Server::Auth.new(@stream)
+    class << stream
+      attr_accessor :remote_domain
+    end
+    stream.remote_domain = 'wonderland.lit'
   end
 
-  describe 'external auth' do
-    it 'passes with empty authzid' do
-      @stream.expect(:remote_domain, 'wonderland.lit')
-      @stream.expect(:cert_domain_matches?, true, ['wonderland.lit'])
-      @stream.expect(:write, nil, ['<success xmlns="urn:ietf:params:xml:ns:xmpp-sasl"/>'])
-      @stream.expect(:advance, nil, [Vines::Stream::Server::FinalRestart])
-      @stream.expect(:reset, nil)
-      @stream.expect(:authentication_mechanisms, ['EXTERNAL'])
-      node = node(%Q{<auth xmlns="urn:ietf:params:xml:ns:xmpp-sasl" mechanism="EXTERNAL">=</auth>})
-      @state.node(node)
-      assert @stream.verify
+  describe 'when given a valid authzid' do
+    before do
+      stream.expect :cert_domain_matches?, true, ['wonderland.lit']
+      stream.expect :write, nil, ['<success xmlns="urn:ietf:params:xml:ns:xmpp-sasl"/>']
+      stream.expect :advance, nil, [Vines::Stream::Server::FinalRestart]
+      stream.expect :reset, nil
+      stream.expect :authentication_mechanisms, ['EXTERNAL']
     end
 
-    it 'passes when authzid matches from domain' do
-      @stream.expect(:remote_domain, 'wonderland.lit')
-      @stream.expect(:cert_domain_matches?, true, ['wonderland.lit'])
-      @stream.expect(:write, nil, ['<success xmlns="urn:ietf:params:xml:ns:xmpp-sasl"/>'])
-      @stream.expect(:advance, nil, [Vines::Stream::Server::FinalRestart])
-      @stream.expect(:reset, nil)
-      @stream.expect(:authentication_mechanisms, ['EXTERNAL'])
-      node = node(%Q{<auth xmlns="urn:ietf:params:xml:ns:xmpp-sasl" mechanism="EXTERNAL">#{Base64.strict_encode64('wonderland.lit')}</auth>})
-      @state.node(node)
-      assert @stream.verify
+    it 'passes external auth with empty authzid' do
+      node = external('=')
+      subject.node(node)
+      stream.verify
     end
 
-    it 'fails when authzid does not match from domain' do
-      @stream.expect(:remote_domain, 'wonderland.lit')
-      @stream.expect(:write, nil, ['</stream:stream>'])
-      @stream.expect(:close_connection_after_writing, nil)
-      @stream.expect(:error, nil, [Vines::SaslErrors::InvalidAuthzid])
-      @stream.expect(:authentication_mechanisms, ['EXTERNAL'])
-      node = node(%Q{<auth xmlns="urn:ietf:params:xml:ns:xmpp-sasl" mechanism="EXTERNAL">#{Base64.strict_encode64('verona.lit')}</auth>})
-      @state.node(node)
-      assert @stream.verify
+    it 'passes external auth with authzid matching from domain' do
+      node = external(Base64.strict_encode64('wonderland.lit'))
+      subject.node(node)
+      stream.verify
+    end
+  end
+
+  describe 'when given an invalid authzid' do
+    before do
+      stream.expect :write, nil, ['</stream:stream>']
+      stream.expect :close_connection_after_writing, nil
+      stream.expect :error, nil, [Vines::SaslErrors::InvalidAuthzid]
+      stream.expect :authentication_mechanisms, ['EXTERNAL']
+    end
+
+    it 'fails external auth with mismatched from domain' do
+      node = external(Base64.strict_encode64('verona.lit'))
+      subject.node(node)
+      stream.verify
     end
   end
 
   private
 
-  def node(xml)
-    Nokogiri::XML(xml).root
+  def external(authzid)
+    node(%Q{<auth xmlns="urn:ietf:params:xml:ns:xmpp-sasl" mechanism="EXTERNAL">#{authzid}</auth>})
   end
 end
