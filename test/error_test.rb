cc3511a3932afbd6e921cff6ff0a93b229b21d3e
David Graham <dgraham@github.com>
2012-09-21 10:25:53 -0600

    Convert to specs.

--- test/error_test.rb
+++ test/error_test.rb
@@ -1,59 +1,58 @@
 # encoding: UTF-8
 
-require 'vines'
-require 'minitest/autorun'
+require 'test_helper'
 
-class ErrorTest < MiniTest::Unit::TestCase
-  def test_sasl_error_without_text
-    expected = %q{<failure xmlns="urn:ietf:params:xml:ns:xmpp-sasl"><temporary-auth-failure/></failure>}
-    assert_equal expected, Vines::SaslErrors::TemporaryAuthFailure.new.to_xml
+describe Vines::XmppError do
+  describe Vines::SaslErrors do
+    it 'does not require a text element' do
+      expected = %q{<failure xmlns="urn:ietf:params:xml:ns:xmpp-sasl"><temporary-auth-failure/></failure>}
+      Vines::SaslErrors::TemporaryAuthFailure.new.to_xml.must_equal expected
+    end
+
+    it 'includes a text element when message is given' do
+      text = %q{<text xml:lang="en">busted</text>}
+      expected = %q{<failure xmlns="urn:ietf:params:xml:ns:xmpp-sasl"><temporary-auth-failure/>%s</failure>} % text
+      Vines::SaslErrors::TemporaryAuthFailure.new('busted').to_xml.must_equal expected
+    end
   end
 
-  def test_sasl_error_with_text
-    text = %q{<text xml:lang="en">busted</text>}
-    expected = %q{<failure xmlns="urn:ietf:params:xml:ns:xmpp-sasl"><temporary-auth-failure/>%s</failure>} % text
-    assert_equal expected, Vines::SaslErrors::TemporaryAuthFailure.new('busted').to_xml
+  describe Vines::StreamErrors do
+    it 'does not require a text element' do
+      expected = %q{<stream:error><internal-server-error xmlns="urn:ietf:params:xml:ns:xmpp-streams"/></stream:error>}
+      Vines::StreamErrors::InternalServerError.new.to_xml.must_equal expected
+    end
+
+    it 'includes a text element when message is given' do
+      text = %q{<text xmlns="urn:ietf:params:xml:ns:xmpp-streams" xml:lang="en">busted</text>}
+      expected = %q{<stream:error><internal-server-error xmlns="urn:ietf:params:xml:ns:xmpp-streams"/>%s</stream:error>} % text
+      Vines::StreamErrors::InternalServerError.new('busted').to_xml.must_equal expected
+    end
   end
 
-  def test_stream_error_without_text
-    expected = %q{<stream:error><internal-server-error xmlns="urn:ietf:params:xml:ns:xmpp-streams"/></stream:error>}
-    assert_equal expected, Vines::StreamErrors::InternalServerError.new.to_xml
-  end
+  describe Vines::StanzaErrors do
+    it 'raises when given a bad type' do
+      node = node('<message/>')
+      -> { Vines::StanzaErrors::BadRequest.new(node, 'bogus') }.must_raise RuntimeError
+    end
 
-  def test_stream_error_with_text
-    text = %q{<text xmlns="urn:ietf:params:xml:ns:xmpp-streams" xml:lang="en">busted</text>}
-    expected = %q{<stream:error><internal-server-error xmlns="urn:ietf:params:xml:ns:xmpp-streams"/>%s</stream:error>} % text
-    assert_equal expected, Vines::StreamErrors::InternalServerError.new('busted').to_xml
-  end
+    it 'raises when given a bad stanza' do
+      node = node('<bogus/>')
+      -> { Vines::StanzaErrors::BadRequest.new(node, 'modify') }.must_raise RuntimeError
+    end
 
-  def test_stanza_error_with_bad_type
-    node = node('<message/>')
-    assert_raises(RuntimeError) { Vines::StanzaErrors::BadRequest.new(node, 'bogus') }
-  end
+    it 'does not require a text element' do
+      error = %q{<error type="modify"><bad-request xmlns="urn:ietf:params:xml:ns:xmpp-stanzas"/></error>}
+      expected = %q{<message from="hatter@wonderland.lit" to="alice@wonderland.lit" type="error">%s</message>} % error
+      node = node(%Q{<message from="alice@wonderland.lit" to="hatter@wonderland.lit"/>})
+      Vines::StanzaErrors::BadRequest.new(node, 'modify').to_xml.must_equal expected
+    end
 
-  def test_stanza_error_with_bad_stanza
-    node = node('<bogus/>')
-    assert_raises(RuntimeError) { Vines::StanzaErrors::BadRequest.new(node, 'modify') }
-  end
-
-  def test_stanza_error_without_text
-    error = %q{<error type="modify"><bad-request xmlns="urn:ietf:params:xml:ns:xmpp-stanzas"/></error>}
-    expected = %q{<message from="hatter@wonderland.lit" to="alice@wonderland.lit" type="error">%s</message>} % error
-    node = node(%Q{<message from="alice@wonderland.lit" to="hatter@wonderland.lit"/>})
-    assert_equal expected, Vines::StanzaErrors::BadRequest.new(node, 'modify').to_xml
-  end
-
-  def test_stanza_error_with_text
-    text = %q{<text xmlns="urn:ietf:params:xml:ns:xmpp-stanzas" xml:lang="en">busted</text>}
-    error = %q{<error type="modify"><bad-request xmlns="urn:ietf:params:xml:ns:xmpp-stanzas"/>%s</error>} % text
-    expected = %q{<message id="42" type="error">%s</message>} % error
-    node = node(%Q{<message id="42"/>})
-    assert_equal expected, Vines::StanzaErrors::BadRequest.new(node, 'modify', 'busted').to_xml
-  end
-
-  private
-
-  def node(xml)
-    Nokogiri::XML(xml).root
+    it 'includes a text element when message is given' do
+      text = %q{<text xmlns="urn:ietf:params:xml:ns:xmpp-stanzas" xml:lang="en">busted</text>}
+      error = %q{<error type="modify"><bad-request xmlns="urn:ietf:params:xml:ns:xmpp-stanzas"/>%s</error>} % text
+      expected = %q{<message id="42" type="error">%s</message>} % error
+      node = node(%Q{<message id="42"/>})
+      Vines::StanzaErrors::BadRequest.new(node, 'modify', 'busted').to_xml.must_equal expected
+    end
   end
 end
