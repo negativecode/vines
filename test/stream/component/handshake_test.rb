491c107014727a560a64776f3f556080e55960fe
David Graham <dgraham@github.com>
2012-09-22 19:38:29 -0600

    Convert to specs.

--- test/stream/component/handshake_test.rb
+++ test/stream/component/handshake_test.rb
@@ -2,47 +2,51 @@
 
 require 'test_helper'
 
-class HandshakeTest < MiniTest::Unit::TestCase
-  def setup
-    @stream = MiniTest::Mock.new
-    @state = Vines::Stream::Component::Handshake.new(@stream)
+describe Vines::Stream::Component::Handshake do
+  subject      { Vines::Stream::Component::Handshake.new(stream) }
+  let(:stream) { MiniTest::Mock.new }
+
+  describe 'when invalid element is received' do
+    it 'raises a not-authorized stream error' do
+      node = node('<message/>')
+       -> { subject.node(node) }.must_raise Vines::StreamErrors::NotAuthorized
+    end
   end
 
-  def test_invalid_element
-    node = node('<message/>')
-    assert_raises(Vines::StreamErrors::NotAuthorized) { @state.node(node) }
+  describe 'when handshake with no text is received' do
+    it 'raises a not-authorized stream error' do
+      stream.expect :secret, 'secr3t'
+      node = node('<handshake/>')
+      -> { subject.node(node) }.must_raise Vines::StreamErrors::NotAuthorized
+      stream.verify
+    end
   end
 
-  def test_missing_text
-    @stream.expect(:secret, 'secr3t')
-    node = node('<handshake/>')
-    assert_raises(Vines::StreamErrors::NotAuthorized) { @state.node(node) }
-    assert @stream.verify
+  describe 'when handshake with invalid secret is received' do
+    it 'raises a not-authorized stream error' do
+      stream.expect :secret, 'secr3t'
+      node = node('<handshake>bogus</handshake>')
+      -> { subject.node(node) }.must_raise Vines::StreamErrors::NotAuthorized
+      stream.verify
+    end
   end
 
-  def test_invalid_secret
-    @stream.expect(:secret, 'secr3t')
-    node = node('<handshake>bogus</handshake>')
-    assert_raises(Vines::StreamErrors::NotAuthorized) { @state.node(node) }
-    assert @stream.verify
-  end
+  describe 'when good handshake is received' do
+    let(:router) { MiniTest::Mock.new }
 
-  def test_valid_secret
-    router = MiniTest::Mock.new
-    router.expect(:<<, nil, [@stream])
-    @stream.expect(:router, router)
-    @stream.expect(:secret, 'secr3t')
-    @stream.expect(:write, nil, ['<handshake/>'])
-    @stream.expect(:advance, nil, [Vines::Stream::Component::Ready.new(@stream)])
-    node = node('<handshake>secr3t</handshake>')
-    @state.node(node)
-    assert @stream.verify
-    assert router.verify
-  end
+    before do
+      router.expect :<<, nil, [stream]
+      stream.expect :router, router
+      stream.expect :secret, 'secr3t'
+      stream.expect :write, nil, ['<handshake/>']
+      stream.expect :advance, nil, [Vines::Stream::Component::Ready.new(stream)]
+    end
 
-  private
-
-  def node(xml)
-    Nokogiri::XML(xml).root
+    it 'completes the handshake and advances the stream into the ready state' do
+      node = node('<handshake>secr3t</handshake>')
+      subject.node(node)
+      stream.verify
+      router.verify
+    end
   end
 end
